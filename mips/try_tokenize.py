9644015501e074aadcca13218ac1369bcd42a6d4
Andrew Lamoureux <andrew@vector35.com>
2018-05-11 01:10:28 -0400

    mips: further attempt to tokenize all instructions

--- mips/try_tokenize.py
+++ mips/try_tokenize.py
@@ -24,11 +24,11 @@ signal.signal(signal.SIGINT, int_handler)
 signal.alarm(4)
 
 def tokenize(string):
-	aliases = ['$zero':0, '$at':1, '$v0':2, '$v1':3, \
+	gpr_aliases = {'$zero':0, '$at':1, '$v0':2, '$v1':3, \
 			'$a0':4, '$a1':5, '$a2':6, '$a3':7, \
-			'$t0':8, '$t1':9, '$t2', '$t3', '$t4', '$t5', '$t6', '$t7', '$t8', '$t9' \
-			'$s0', '$s1', '$s2', '$s3', '$s4', '$s5', '$s6', '$s7', \
-			'$k0', '$k1', '$gp', '$sp', '$fp', '$ra']:
+			'$t0':8, '$t1':9, '$t2':10, '$t3':11, '$t4':12, '$t5':13, '$t6':14, '$t7':15, '$t8':24, '$t9':25, \
+			'$s0':16, '$s1':17, '$s2':18, '$s3':19, '$s4':20, '$s5':21, '$s6':22, '$s7':23, '$s8':30, \
+			'$k0':26, '$k1':27, '$gp':28, '$sp':29, '$fp':30, '$ra':31}
 
 	tokens = instr.split()
 
@@ -47,27 +47,17 @@ def tokenize(string):
 		if string.startswith(' '):
 			eat = 1
 
-		# "f" registers
+		# "f" registers $f0,$f1,...,$f31
 		elif re.match(r'^\$f\d+', string):
 			tmp = re.match(r'^(\$f\d+)', string).group(1) # eg: $f2
 			result.append( ('FREG',int(tmp[2:])) )
 			eat = len(tmp)
-		# "t" registers
-		elif re.match(r'^\$t\d+', string):
-			tmp = re.match(r'^(\$t\d+)', string).group(1) # eg: $t0
-			result.append( ('TREG',int(tmp[2:])) )
-			eat = len(tmp)
-		# "s" registers
-		elif re.match(r'^\$s\d+', string):
-			tmp = re.match(r'^(\$s\d+)', string).group(1) # eg: $s0
-			result.append( ('SREG',int(tmp[2:])) )
-			eat = len(tmp)
-		# "w" registers
+		# "w" registers $w0,$w1,...,$w31
 		elif re.match(r'^\$w\d+', string):
 			tmp = re.match(r'^(\$w\d+)', string).group(1) # eg: $w0
 			result.append( ('WREG',int(tmp[2:])) )
 			eat = len(tmp)
-		# "ac" registers
+		# "ac" registers $ac0,$ac1,$ac2,$ac3
 		elif re.match(r'^\$ac\d+', string):
 			tmp = re.match(r'^(\$ac\d+)', string).group(1) # eg: $w0
 			result.append( ('ACREG',int(tmp[3:])) )
@@ -79,17 +69,9 @@ def tokenize(string):
 		elif re.match(r'^\$zero', string):
 			result.append( ('GPREG',0) )
 			eat = 5
-		# ugh
-		elif string[0:3] in ['$at', '$v0', '$v1', '$a0', '$a1', '$a2', '$a3', \
-			'$t0', '$t1', '$t2', '$t3', '$t4', '$t5', '$t6', '$t7', '$t8', '$t9' \
-			'$s0', '$s1', '$s2', '$s3', '$s4', '$s5', '$s6', '$s7', \
-			'$k0', '$k1', '$gp', '$sp', '$fp', '$ra']:
-
-
-			result.append( ('GPREG',1) )
-			eat = 3
-		elif string[0:3] in ['$v0', '$v1']:
-			result.append( ('GPREG',2+int(string[2])) )
+		# aliases
+		elif string[0:3] in gpr_aliases:
+			result.append( ('GPREG',gpr_aliases[string[0:3]]) )
 			eat = 3
 		# hex numeric literals
 		elif re.match(r'^-?0x[a-fA-F0-9]+', string):
@@ -101,9 +83,14 @@ def tokenize(string):
 			tmp = re.match(r'(^-?\d+)', string).group(1)
 			result.append( ('NUM', int(tmp)) )
 			eat = len(tmp)
+		# dollar literals
+		elif re.match(r'^\$\d+', string):
+			tmp = re.match(r'^(\$\d+)', string).group(1)
+			result.append( ('DOLLAR_NUM', int(tmp[1:])) )
+			eat = len(tmp)
 		# punctuation, operators
-		elif string[0] in list('(),.*+-'):
-			result.append( ('PUNC',string[0]) )
+		elif string[0] in list('[](),.*+-'):
+			result.append( (string[0],string[0]) )
 			eat = 1			
 		else:
 			raise Exception('dunno what to do with: %s (original input: %s)' % (string, string_))
@@ -123,14 +110,22 @@ for instr_word in xrange(0,END):
 
 	instr = cbuf.value
 	tokens = tokenize(instr)
-	print tokens
+	syntax = tokens[0][1] + ' ' + ' '.join(map(lambda x: x[0], tokens[1:]))
+
+	if syntax in seen:
+		seen[syntax] += 1
+	else:
+		seen[syntax] = 1
 
 	if status:
-		print 'on instr_word: 0x%08X, collected %d opcodes so far' % (instr_word, len(seen))
+		print 'on instr_word: 0x%08X, collected %d syntaxes so far' % (instr_word, len(seen))
 		status = False
 		signal.alarm(4)
 
 	if qearly:
 		break
 
-print 'collected opcodes: ', '\n'.join(sorted(seen))
+for sig in sorted(seen):
+	print '"%s": %d,' % (sig, seen[sig])
+
+print '(%d signatures total)' % len(seen)
