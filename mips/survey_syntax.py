1949a030170bcb3aa066393f44922a3b6baac8ab
Andrew Lamoureux <andrew@vector35.com>
2018-05-12 01:34:00 -0400

    mips: updated survey_syntax.py

--- mips/survey_syntax.py
+++ mips/survey_syntax.py
@@ -6,6 +6,7 @@ import struct
 import ctypes
 import random
 import signal
+import time
 
 status = False
 def alarm_handler(x,y):
@@ -109,16 +110,22 @@ if sys.argv[2:]:
 	END = int(sys.argv[2], 16)
 	print "doing %08X to %08X" % (START, END)
 
-for instr_word in xrange(START,END):
-#while 1:
-#	instr_word = random.randint(0,0xFFFFFFFF)
+t0 = time.time()
 
-	data = struct.pack('<I', instr_word)
+for insword in xrange(START,END):
+#while 1:
+#	insword = random.randint(0,0xFFFFFFFF)
+
+	data = struct.pack('<I', insword)
 	gofer.get_disasm_capstone(data, 4, ctypes.byref(cbuf))
 
 	instr = cbuf.value
 	tokens = tokenize(instr)
 	syntax = tokens[0][1] + ' ' + ' '.join(map(lambda x: x[0], tokens[1:]))
+
+	#if syntax in ['lwc2 DOLLAR_NUM , ( )', 'jialc NUM ,']:
+	#	print 'wtf? %08X: %s' % (insword, instr)
+	#	sys.exit(-1)
 
 	if syntax in seen:
 		seen[syntax] += 1
@@ -126,7 +133,11 @@ for instr_word in xrange(START,END):
 		seen[syntax] = 1
 
 	if status:
-		print 'on instr_word: 0x%08X, collected %d syntaxes so far' % (instr_word, len(seen))
+		t_elapsed = int(time.time() - t0);
+		ips = insword / t_elapsed
+		remaining = int((0x100000000 - insword) * (1.0/ips))
+		print 'insword:%08X opcs:%d t:%ds ips:%d left:%dm' % \
+			(insword, len(seen), t_elapsed, int(ips), remaining/60)
 		status = False
 		signal.alarm(4)
 
