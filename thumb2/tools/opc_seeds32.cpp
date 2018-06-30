7a06f3d425847a856f17ae0c434ea0cba0a190ff
Andrew Lamoureux <andrew@vector35.com>
2018-06-30 01:18:05 -0400

    thumb2: fixed 32-bit opc seeds finder

--- thumb2/tools/opc_seeds32.cpp
+++ thumb2/tools/opc_seeds32.cpp
@@ -18,6 +18,11 @@ void cb_alarm(int sig) {
 	show_status = true;
 }
 
+volatile sig_atomic_t breakout = false;
+void cb_int(int sig) {
+	breakout = true;
+}
+
 /******************************/
 /* main */
 /******************************/
@@ -25,18 +30,19 @@ void cb_alarm(int sig) {
 int main(int ac, char **av)
 {
 	signal(SIGALRM, cb_alarm);
+	signal(SIGINT, cb_int);
 	alarm(8);
 
 	unordered_map<string, uint32_t> str2enc;
 	unordered_map<string, int> str2cnt;
 
-	for(uint64_t iw=0; iw<0x100000000; ++iw) {
+	for(uint64_t iw=0x0; iw<0x100000000; ++iw) {
 		char distxt[64];
 		int size = disasm((uint8_t *)&iw, distxt);
 
 		if(size != 4)
 			continue;
-
+		
 		if(str2enc.find(distxt) != str2enc.end()) {
 			str2cnt[distxt] += 1;
 		}
@@ -49,15 +55,18 @@ int main(int ac, char **av)
 			printf("current instruction word: %08llX\n", iw);
 			show_status = false;
 			alarm(8);
-		}		
+		}
+
+		if(breakout)
+			break;
 	}
 
 	int total = 0;
 	for(auto it = str2enc.begin(); it != str2enc.end(); it++) {
 		string opc = it->first;
-		uint16_t enc = it->second;
+		uint32_t enc = it->second;
 		int amt = str2cnt[opc];
-		printf("\"%s\": \"\\x%02x\\x%02X\\%02X\\%02X\" // %d total\n", opc.c_str(),
+		printf("\"%s\": \"\\x%02x\\x%02X\\x%02X\\x%02X\" // %d total\n", opc.c_str(),
 			enc & 0xFF, (enc & 0xFF00)>>8, (enc & 0xFF0000)>>16, (enc & 0xFF000000)>>24, amt);
 
 		total += amt;
