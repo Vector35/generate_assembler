6cc592762b0482911b5d318791500de962f0f7fb
Andrew Lamoureux <andrew@vector35.com>
2018-07-01 14:12:21 -0400

    thumb2: tests

---
+++ thumb2/tools/gen_tests32.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import re
+import sys
+import struct
+import ctypes
+import itertools
+import signal
+import time
+import struct
+from random import shuffle
+
+import common
+
+###############
+# read files
+###############
+syn2seed = {}
+syn2mask = {}
+lines = None
+with open('syn_pats32.txt') as fp:
+	lines = fp.readlines()
+for l in lines:
+	# {                "adcs GPR , GPR",{0x4140,0x003F}}, // 0100000101xxxxxx
+	m = re.search(r'"(.*)",{(.*),(.*)}}', l)
+	if m:
+		syn2seed[m.group(1)] = int(m.group(2),16)
+		syn2mask[m.group(1)] = int(m.group(3),16)
+
+###############
+# go!
+###############
+
+seen = {}
+fuzz = common.fuzz6()
+
+syntaxes = sorted(syn2seed)
+
+for syn in syntaxes:
+	shuffle(fuzz)
+	seed = syn2seed[syn]
+	mask = syn2mask[syn]
+	distxt = common.disasm32(seed)
+	syn = common.syntax_from_string(distxt)
+
+	print "// examples of %s" % syn
+
+	collection = 0
+	for f in fuzz:
+		seed2 = seed ^ (mask & f)
+		distxt2 = common.disasm32(seed2)
+		syn2 = common.syntax_from_string(distxt2)
+
+		if syn != syn2:
+			continue
+
+		if distxt2 in seen:
+			continue
+
+		print '%08X,"%s"' % (seed2, distxt2)
+		seen[distxt2] = True
+
+		collection += 1
+		if collection >= 4:
+			break;
+
